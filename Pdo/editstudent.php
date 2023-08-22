f18d50eebb419196c0d6ece1d2c010d9001d7174
leobrice <mushibrayson07@gmail.com>
2023-08-22 15:46:25 +0300

    Add datatables

--- Pdo/editstudent.php
+++ Pdo/editstudent.php
@@ -3,7 +3,7 @@
     <div class="modal-dialog">
         <div class="modal-content">
             <div class="modal-header">
-                <h5 class="modal-title" id="exampleModalLabel">Modal title</h5>
+                <h5 class="modal-title" id="exampleModalLabel">Edit Student</h5>
                 <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
             </div>
             <div class="modal-body">
@@ -29,6 +29,47 @@
                     <div class="modal-footer">
                         <button type="clear" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
                         <button type="submit" name="submit"  class="btn btn-primary">Save</button>
+                    </div>
+                </form>
+            </div>
+        </div>
+    </div>
+</div>
+
+
+<div class="modal fade" id="delete_<?php echo $value['student_id'] ?>" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
+    <div class="modal-dialog">
+        <div class="modal-content">
+            <div class="modal-header">
+                <h5 class="modal-title" id="exampleModalLabel">Delete Student </h5>
+                <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+            </div>
+            <div class="modal-body">
+                <div class="alert alert-danger">
+                        <p class="text-center">Delete this Data</p>
+                </div>
+                <form action="./action/edit.php?id=<?php echo $value['student_id']; ?>" method="post">
+                    <div class="row">
+                        <label for="std_name" class="col-3">Name</label>
+                        <div class="col-9">
+                            <input type="text" class="form-control" name="std_name" id="" value="<?php echo $value['student_name'] ?>" readonly>
+                        </div>
+                    </div>
+                    <div class="row">
+                        <label for="email" class="col-3">Email</label>
+                        <div class="col-9">
+                            <input type="text" class="form-control" name="email" id="" value="<?php echo $value['student_email'] ?>" readonly> 
+                        </div>
+                    </div>
+                    <div class="row">
+                        <label for="age" class="col-3">Age</label>
+                        <div class="col-9">
+                            <input type="text" class="form-control" name="age" id="" value="<?php echo $value['student_age'] ?>" readonly>
+                        </div>
+                    </div>
+                    <div class="modal-footer">
+                        <button type="clear" class="btn btn-secondary" data-bs-dismiss="modal"><span><i class="fa fa-ban" ></i></span></button>
+                        <button type="submit" name="submit"  class="btn btn-danger"><span><i class="fa fa-trash" ></i></span></button>
                     </div>
                 </form>
             </div>
--- Pdo/index.php
+++ Pdo/index.php
@@ -1,9 +1,9 @@
 <?php
 include "./connection.php";
-    session_start();
-    // $_SESSION['message']="";
-    error_reporting(E_ALL);
-    ini_set('display_errors', 1);
+session_start();
+// $_SESSION['message']="";
+error_reporting(E_ALL);
+ini_set('display_errors', 1);
 ?>
 <!DOCTYPE html>
 <html lang="en">
@@ -12,8 +12,16 @@ include "./connection.php";
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title> PDO Crud</title>
+
     <!-- Bootstrap 5 -->
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
+    <!-- <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous"> -->
+
+    <!-- Font awesome -->
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css">
+
+    <!-- Datatables -->
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/5.3.0/css/bootstrap.min.css">
+    <link rel="stylesheet" href="https://cdn.datatables.net/1.13.6/css/dataTables.bootstrap5.min.css">
 
 </head>
 
@@ -26,24 +34,24 @@ include "./connection.php";
             <div>
                 <button type="button" class="btn btn-success" data-bs-toggle="modal" data-bs-target="#addnew">Add new</button>
             </div>
-            <table class="table table-striped table-bordered">                
-                    <?php     if (!empty($_SESSION['message'])): ?>
+            <table class="table table-striped table-bordered" id="example">
+                <?php if (!empty($_SESSION['message'])) : ?>
                     <div class="alert alert-success text-center">
-                        <?php 
-                            echo $_SESSION['message'];
-                            unset($_SESSION['message']);
+                        <?php
+                        echo $_SESSION['message'];
+                        unset($_SESSION['message']);
                         ?>
                     </div>
-                    <?php endif; ?>
-                <thead> Students </thead>
-                <tr>
+                <?php endif; ?>                
+                <thead>
                     <th>Id</th>
                     <th>Name</th>
                     <th>Email</th>
                     <th>Age</th>
                     <th>Last Edit</th>
-                    <th>Action</th>
-                </tr>
+                    <th>Edit</th>
+                    <th>Delete</th>
+                </thead>
                 <?php
                 try {
 
@@ -62,14 +70,19 @@ include "./connection.php";
                             <td><?php echo $value['addedAt'] ?></td>
                             <td>
                                 <a href="#edit_<?php echo $value['student_id'] ?>" class="btn btn-info" data-bs-toggle="modal">
-                                    Edit
+                                    <span><i class="fa fa-pencil"></i> </span>
                                 </a>
+                            </td>
+                            <td>
                                 <a href="#delete_<?php echo $value['student_id'] ?>" class="btn btn-danger" data-bs-toggle="modal">
-                                 Delete
+                                    <span><i class="fa fa-trash"></i></span>
                                 </a>
                             </td>
                         </tr>
-                        <?php include "./editstudent.php"; ?>
+                        <?php
+                        include "./editstudent.php";
+                        ?>
+
                 <?php
                     }
                 } catch (PDOException $th) {
@@ -86,6 +99,17 @@ include "./connection.php";
 include "./addstudent.php";
 
 ?>
+
+
+<!-- Datatables -->
+
+<script src="https://code.jquery.com/jquery-3.7.0.js"></script>
+<script src="https://cdn.datatables.net/1.13.6/js/jquery.dataTables.min.js"></script>
+<script src="https://cdn.datatables.net/1.13.6/js/dataTables.bootstrap5.min.js"></script>
+<script>
+    new DataTable('#example');
+</script>
+
 <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.10.2/dist/umd/popper.min.js" integrity="sha384-7+zCNj/IqJ95wo16oMtfsKbZ9ccEh31eOz1HGyDuCQ6wgnyJNSYdrPa03rtR1zdB" crossorigin="anonymous"></script>
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.min.js" integrity="sha384-QJHtvGhmr9XOIpI6YVutG+2QOK9T+ZnN4kzFN1RtK3zEFEIsxhlmWl5/YESvpZ13" crossorigin="anonymous"></script>
 
